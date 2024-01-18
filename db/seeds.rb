805efcb2fa292373ba7834bcb4dadb401b1ef01d
Aurelien Simon <aureliensimon@hotmail.com>
2024-01-18 21:31:46 +0100

    solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+2.times do
+  Flat.create!(
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3,
+    picture_url: "https://source.unsplash.com/random/200*200/?house"
+  )
+  Flat.create!(
+    name: 'L\'appartement de Tal (absence de lit)',
+    address: 'Sanary sur mer',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 5,
+    picture_url: "https://source.unsplash.com/random/200*200/?flat"
+  )
+  Flat.create!(
+    name: 'Chalet savoyard',
+    address: 'Courchevel',
+    description: 'A sweet and cosy wood house surrounded by snow and trees, in the middle of the old village of Courchevel',
+    price_per_night: 125,
+    number_of_guests: 10,
+    picture_url: "https://source.unsplash.com/random/200*200/?house,green"
+  )
+  Flat.create!(
+    name: 'Bungalow in front of the see',
+    address: 'Nice',
+    description: 'A perfect place for your holidays in front of the Promenade des Anglais in Nice, to enjoy ice creams, sunset and bathing in hot water',
+    price_per_night: 100,
+    number_of_guests: 6,
+    picture_url: "https://source.unsplash.com/random/200*200/?house,city"
+  )
+end
